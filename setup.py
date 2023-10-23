69e4d6a0248df6ab1978605a28e0b34534162c95
drEast <felix.drost@helmholtz-muenchen.de>
2023-10-23 09:21:21 +0200

    Pip installable setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tcr_benchmark",
+    version="1.0.0",
+    packages=find_packages(include=["tcr_benchmark", "tcr_benchmark.*"]),
+    install_requires=[
+        "pandas",
+        "numpy",
+        "scikit-learn",
+        "scipy",
+        "requests",
+        "PyYAML",
+        "tqdm"
+    ],
+    extras_require={
+        "epytope": [
+            "epytope @ git+https://github.com/SchubertLab/epytope"
+            ],
+       # "reproducability": [
+       #
+       # ] # TODO: add exact versions of the used tools
+    }
+)
