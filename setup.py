48c437734f4078a5b25bbbffef1250ba238bb4c9
annachernysheva179 <131196587+annachernysheva179@users.noreply.github.com>
2023-12-13 11:56:00 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import setup, find_packages
+import pkg_resources
+import pathlib
 try: # for pip >= 10
     from pip._internal.req import parse_requirements
 except ImportError: # for pip <= 9.0.3
     from pip.req import parse_requirements
 
-install_reqs = parse_requirements("requirements.txt", session=False)
-reqs = [str(ir.req) for ir in install_reqs]
-
-print(requirements)
+with pathlib.Path('requirements.txt').open() as requirements_txt:
+    reqs = [
+        str(requirement)
+        for requirement
+        in pkg_resources.parse_requirements(requirements_txt)
+    ]
 
 setup(
     name="tcr_benchmark",
