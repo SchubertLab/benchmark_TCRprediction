df486a8f0b427d0c0aa47f481a89add69044a482
annachernysheva179 <131196587+annachernysheva179@users.noreply.github.com>
2023-12-20 14:58:48 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ with open('epytope_requirements.txt') as f:
 # Filter out comments and empty lines
 requirements = [req.strip() for req in requirements if req.strip() and not req.strip().startswith('#')]
 requirements = [req for req in requirements if '==' in req or '>=' in req or '<=' in req or '>' in req or '<' in req]
+for i in range(len(requirements)):
+    print("lala")
+    print("requirements[i])
 
 setup(
     name="tcr_benchmark",
