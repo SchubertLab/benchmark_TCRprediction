fe8c8515673b5293eea6dfb1e322aa7c3886accb
annachernysheva179 <131196587+annachernysheva179@users.noreply.github.com>
2024-02-01 11:48:16 +0100

    Update run_base_experiments.sh

--- scripts/run_base_experiments.sh
+++ scripts/run_base_experiments.sh
@@ -2,6 +2,6 @@
 python -m tcr_benchmark.study.benchmark \
         --dataset_selection $1 \
         --predictor_selection all \
-        --path_config_yaml ${CODE_PATH}/config/cluster_config.yaml \
+        --path_config_yaml ${CODE_PATH}/config/config_docker.yaml \
 	--path_out ${RESULT_PATH}/base_experiment_$1.csv
 
